eed4afe59fa7ba4707218c09b6c1b59682741bbe
Doug <douglas.b.hill@gmail.com>
2022-09-04 16:29:52 -0500

    Add Wasm target support (#88)
    Added conditional __WASM__ so that entropy can be defined at link time for wasm targets for clang + wasi-sdk for browser.
    
    Co-authored-by: Ulrich Telle <ulrich.telle@gmx.de>

--- src/chacha20poly1305.c
+++ src/chacha20poly1305.c
@@ -387,9 +387,17 @@ static size_t entropy(void* buf, size_t n)
 #endif
   return read_urandom(buf, n);
 }
+#elif defined(__WASM__)
+extern size_t getentropy(void* buf, size_t n);
+
+static size_t entropy(void* buf, size_t n)
+{
+  return (getentropy(buf, n) == 0) ? n : 0;
+}
 #else
 # error "Secure pseudorandom number generator not implemented for this OS"
 #endif
+
 
 /*
  * ChaCha20 random number generator
