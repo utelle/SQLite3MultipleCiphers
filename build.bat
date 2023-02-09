d2f4bfbf1d04a21b5947eed6ff5f276125989866
jammerxd <8965289+jammerxd@users.noreply.github.com>
2023-02-08 20:44:02 -0600

    update .gitignore, add proof-of-concept CMakeLists.txt, add build.bat for example of building using cmake on windows. added CMAKE parameter WITH-ICU to specify build with ICU

---
+++ build.bat
@@ -0,0 +1,6 @@
+cmake -G "Visual Studio 17 2022" -A Win32 -S . -B "x86"
+cmake -G "Visual Studio 17 2022" -A x64 -S . -B "x64"
+cmake --build x86 . --config Debug
+cmake --build x86 . --config Release
+cmake --build x64 . --config Debug
+cmake --build x64 . --config Release
