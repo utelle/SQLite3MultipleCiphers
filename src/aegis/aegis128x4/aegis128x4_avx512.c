158d582322db60994c1a4ad2082dcef614e9ca8b
utelle <ulrich.telle@gmx.de>
2026-03-16 22:49:19 +0100

    Add back target option evex512 for clang 18 to 21
    At least with zig using clang 18 to 21 the target option evex512 needs to be specified. To be on the safe side this target option is added back for those clang versions.

--- src/aegis/aegis128x4/aegis128x4_avx512.c
+++ src/aegis/aegis128x4/aegis128x4_avx512.c
@@ -23,8 +23,11 @@
 #ifdef HAVE_VAESINTRIN_H
 
 #ifdef __clang__
-#  pragma clang attribute push(__attribute__((target("vaes,avx512f"))), \
-                                             apply_to = function)
+#  if __clang_major__ >= 18 && __clang_major__ < 22
+#    pragma clang attribute push(__attribute__((target("aes,vaes,avx512f,evex512"))), apply_to=function)
+#  else
+#    pragma clang attribute push(__attribute__((target("aes,vaes,avx512f"))), apply_to=function)
+#  endif
 #elif defined(__GNUC__)
 #  pragma GCC target("vaes,avx512f")
 #endif
