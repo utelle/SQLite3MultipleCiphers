26f0f6bf679a90981bd7c2591cc7505aef0e2c89
utelle <ulrich.telle@gmx.de>
2026-05-25 17:42:19 +0200

    Extend python script, adjust build workflow

--- scripts/build_context.py
+++ scripts/build_context.py
@@ -21,14 +21,27 @@ def main():
     sqlite_version = config["sqlite"]["version"]
 
     # URLs aus config (kein Hardcoding!)
-    official = config["sqlite"]["sources"]["official"]["url"]
-    github = config["sqlite"]["sources"]["github"]["url"]
+    sqlite_official = config["sqlite"]["sources"]["official"]["url"]
+    sqlite_github = config["sqlite"]["sources"]["github"]["url"]
+
+    icu_version = config["icu"]["version"]
+    icu_official = config["icu"]["sources"]["official"]["url"]
 
     ctx = {
         "SQLITE_VERSION": sqlite_version,
         "SQLITE_VERSION_RAW": sqlite_raw(sqlite_version),
-        "SQLITE_URL_OFFICIAL": official,
-        "SQLITE_URL_GITHUB": github,
+        "SQLITE_URL_OFFICIAL": sqlite_official,
+        "SQLITE_URL_GITHUB": sqlite_github,
+        "ICU_VERSION": icu_version,
+        "ICU_TEMPLATE": icu_official,
+        "ICU_URL_WIN32": icu_url_template.format(
+            ICU_VERSION=icu_version,
+            PLATFORM="win32"
+        ),
+        "ICU_URL_WIN64": icu_url_template.format(
+            ICU_VERSION=icu_version,
+            PLATFORM="win64"
+        ),
     }
 
     with open(os.environ["GITHUB_ENV"], "a") as f:
